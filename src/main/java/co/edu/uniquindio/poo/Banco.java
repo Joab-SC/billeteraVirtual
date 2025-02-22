69418a74f8610d315bc5f6d631527833d4725633
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2025-02-22 09:20:32 -0500

    Se agregan comentarios en los métodos de banco

--- src/main/java/co/edu/uniquindio/poo/Banco.java
+++ src/main/java/co/edu/uniquindio/poo/Banco.java
@@ -43,10 +43,17 @@ public class Banco {
         this.billeteras = billeteras;
     }
 
+
+    /**
+     * Crear un usario dados todos sus
+     */
     public Usuario CrearUsuario(String nombre, String direccion, String numeroIdentificacion, String correo, String contrasena) {
         return new Usuario(nombre, direccion, numeroIdentificacion, correo, contrasena, null);
     }
 
+    /**
+     * Actualizar un usuario dados todos sus atributos -> void
+     */
     public void actualizarUsuario(Usuario usuario, String nombre, String direccion, String numeroIdentificacion, String correo, String contrasena){
         usuario.setNombre(nombre);
         usuario.setDireccion(direccion);
@@ -55,16 +62,28 @@ public class Banco {
         usuario.setContrasena(contrasena);
     }
 
+    /**
+     * Agregar un usuario a la lista de ususarios -> void
+     */
     public void agregarUsuario(Usuario usuario){
         usuarios.add(usuario);
     }
 
+    /**
+     * Eliminar un ususario de la lista de usuarios -> void
+     */
     public void eliminarUsuario(Usuario usuario){
         usuarios.remove(usuario);
     }
 
+    /**
+     * Crear la billetera con sus respectivos atributos -> Billetera
+     */
     public void crearBilletera(BilleteraVirtual billeraVirtual){}
 
+    /**
+     * Generar un némero aleatorio con una cantidad de digitos predeterminada -> String
+     */
     public static String generarNumeroAleatorio(int digitos){
         StringBuilder numeroAleatorio = new StringBuilder();
         int digito;
